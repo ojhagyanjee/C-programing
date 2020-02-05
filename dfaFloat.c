9007dccd7250acd5608e710e7b43899cb6c16025
Gyanjee Ojha <43068946+ojhagyanjee@users.noreply.github.com>
2020-02-05 18:32:40 +0530

    Add files via upload

---
+++ dfaFloat.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+void main()
+{
+	int i,state;
+	char a[5][3]={1,2,2,1,3,2,2,2,2,4,2,2,4,2,2};
+	char b[10];
+	i=0;state=0;
+	
+	printf("Enter Number\n");
+	scanf("%s",b);
+	//gets(b);
+	
+	while(b[i]!=NULL)
+	{
+		if(isdigit(b[i]))
+		{
+			state=a[state][0];
+		}
+		else if(b[i]=='.')
+		{
+			state=a[state][1];
+		}
+		else state=a[state][2];
+		
+		i++;
+	}
+	if((state==1)||(state==4))
+	printf("Accepted\n");
+	else
+	printf("rejected\n");
+}
